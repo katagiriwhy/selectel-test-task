ad3a623bb3c0f0c92b5cb449446e7fba06f672d7
katagiri <karuk.vova2005@mail.ru>
2026-02-17 13:58:02 +0300

    feature: added analyzer and main files

---
+++ analyzer/analyzer.go
@@ -0,0 +1,28 @@
+package analyzer
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "loglint",
+	Doc:  "checks log messages style and safety",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			call, ok := n.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+
+			checkCall(pass, call)
+			return true
+		})
+	}
+	return nil, nil
+}

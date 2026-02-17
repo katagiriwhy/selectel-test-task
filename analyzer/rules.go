9ac297494e3dca2ec9cc6f72442149c8e5115b6b
katagiri <karuk.vova2005@mail.ru>
2026-02-17 13:49:16 +0300

    feature: added other rules

--- analyzer/rules.go
+++ analyzer/rules.go
@@ -1,8 +1,11 @@
 package analyzer
 
 import (
+	"strings"
 	"unicode"
 	"unicode/utf8"
+
+	"github.com/enescakir/emoji"
 )
 
 func checkLowercase(msg string) bool {
@@ -17,6 +20,34 @@ func checkLowercase(msg string) bool {
 func checkEnglish(msg string) bool {
 	for _, r := range msg {
 		if unicode.In(r, unicode.Cyrillic) {
+			return false
+		}
+	}
+	return true
+}
+
+func checkNoSpecialChars(msg string) bool {
+	for _, r := range msg {
+		if unicode.IsSymbol(r) || unicode.IsPunct(r) {
+			if r != ' ' || emoji.Exist(string(r)) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+var sensitiveKeywords = []string{
+	"password",
+	"token",
+	"api_key",
+	"secret",
+}
+
+func checkSensitive(msg string) bool {
+	lower := strings.ToLower(msg)
+	for _, kw := range sensitiveKeywords {
+		if strings.Contains(lower, kw) {
 			return false
 		}
 	}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module selectel
 go 1.25.2
 
 require golang.org/x/tools v0.42.0
+
+require github.com/enescakir/emoji v1.0.0 // indirect
